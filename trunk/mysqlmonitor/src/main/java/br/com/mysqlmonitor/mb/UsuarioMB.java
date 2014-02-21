6051fa684dff58d011ab9bcb284af998d0b0b90f
orlandojsjr <orlandojsjr@f67e390d-3720-4a02-9272-ce47fddf845c>
2014-02-21 17:59:08 +0000

    Adicionado tratamento de erro.

--- trunk/mysqlmonitor/src/main/java/br/com/mysqlmonitor/mb/UsuarioMB.java
+++ trunk/mysqlmonitor/src/main/java/br/com/mysqlmonitor/mb/UsuarioMB.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.mysqlmonitor.mb;
 
 import com.mysqlmonitor.dao.UsuarioDAO;
 import com.mysqlmonitor.entidade.Usuario;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.inject.Inject;
@@ -20,8 +21,9 @@ import javax.inject.Named;
  */
 @Named
 @RequestScoped
-public class UsuarioMB extends Face implements Serializable{
-    @Inject 
+public class UsuarioMB extends Face implements Serializable {
+
+    @Inject
     private UsuarioDAO usuarioDAO;
     @Inject
     private Usuario usuario;
@@ -42,11 +44,16 @@ public class UsuarioMB extends Face implements Serializable{
     public void setConfirmaSenha(String confirmaSenha) {
         this.confirmaSenha = confirmaSenha;
     }
-    
-    public void salvar(){
-        usuarioDAO.salvar(usuario);
-        addMensagem("Cadastro realizado.", FacesMessage.SEVERITY_INFO);
+
+    public void salvar() {
+        try {
+            usuarioDAO.salvar(usuario);
+            addMensagem("Cadastro realizado.", FacesMessage.SEVERITY_INFO);
+        } catch (Exception ex) {
+            addMensagem("Erro.", FacesMessage.SEVERITY_INFO);
+            Logger.getLogger(UsuarioMB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
-    
+
 }
