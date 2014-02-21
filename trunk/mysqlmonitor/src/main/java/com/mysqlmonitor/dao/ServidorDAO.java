96a3cb1c7a9ecfbd89df005eadf169ad6662a144
orlandojsjr <orlandojsjr@f67e390d-3720-4a02-9272-ce47fddf845c>
2014-02-21 14:07:01 +0000

    Novo metodo findAll().

--- trunk/mysqlmonitor/src/main/java/com/mysqlmonitor/dao/ServidorDAO.java
+++ trunk/mysqlmonitor/src/main/java/com/mysqlmonitor/dao/ServidorDAO.java
@@ -3,23 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.mysqlmonitor.dao;
 
 import com.mysqlmonitor.entidade.GrupoServidor;
 import com.mysqlmonitor.entidade.Servidor;
+import java.util.List;
 
 /**
  *
  * @author orlando
  */
-public class ServidorDAO extends GenericDAO{
-    
-    public void salvar(Servidor servidor) throws Exception{
+public class ServidorDAO extends GenericDAO {
+
+    public void salvar(Servidor servidor) throws Exception {
         super.salvar(servidor);
     }
-    
-    public boolean existeServidorMasterNo(GrupoServidor grupoServidor) throws Exception{        
-        return !super.listarPorParametrosHQL(Servidor.class, "Select s from Servidor s where s.grupoServidor.idGrupoServidor = :idGrupoServidor and s.tipo = 'MASTER'", 0,0, new Parametro( "idGrupoServidor",grupoServidor.getIdGrupoServidor())).isEmpty();
+
+    public List<Servidor> findAll() throws Exception {
+        return super.listarPorParametrosHQL(Servidor.class, "Select s from Servidor s order by s.grupoServidor.bancoDados", 0, 0);
+    }
+
+    public boolean existeServidorMasterNo(GrupoServidor grupoServidor) throws Exception {
+        return !super.listarPorParametrosHQL(Servidor.class, "Select s from Servidor s where s.grupoServidor.idGrupoServidor = :idGrupoServidor and s.tipo = 'MASTER'", 0, 0, new Parametro("idGrupoServidor", grupoServidor.getIdGrupoServidor())).isEmpty();
     }
 }
