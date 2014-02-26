48e2de97b2e9e3358e0a31fcb05a7c09d9e53875
orlandojsjr <orlandojsjr@users.noreply.github.com>
2014-02-26 19:40:21 +0000

    Pagina cadastro.

--- mysqlmonitor/src/main/java/br/com/mysqlmonitor/mb/UsuarioMB.java
+++ mysqlmonitor/src/main/java/br/com/mysqlmonitor/mb/UsuarioMB.java
@@ -9,20 +9,22 @@ import br.com.mysqlmonitor.Interceptor.Login;
 import com.mysqlmonitor.dao.UsuarioDAO;
 import com.mysqlmonitor.entidade.Usuario;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
+import javax.faces.model.SelectItem;
 import javax.inject.Inject;
-import javax.inject.Named;
 
 /**
  *
  * @author orlando
  */
-@Named
+
 @Login
-@RequestScoped
+@Model
 public class UsuarioMB extends Face implements Serializable {
 
     @Inject
@@ -30,6 +32,9 @@ public class UsuarioMB extends Face implements Serializable {
     @Inject
     private Usuario usuario;
     private String confirmaSenha;
+    private String tab;
+    private List<Usuario> usuarios;
+    private List<SelectItem> tipos;
 
     public Usuario getUsuario() {
         return usuario;
@@ -47,15 +52,65 @@ public class UsuarioMB extends Face implements Serializable {
         this.confirmaSenha = confirmaSenha;
     }
 
+    public void setTab(String tab) {
+        this.tab = tab;
+    }
+
+    public String getTab() {
+        return tab;
+    }
+
+    public List<Usuario> getUsuarios() {
+        if (usuarios == null) {
+            carregarListaUsuario();
+        }
+        return usuarios;
+    }
+
+    public List<SelectItem> getTipos() {
+        if(tipos == null){
+            tipos = new ArrayList<SelectItem>();
+            tipos.add(new SelectItem("ADMIN"));
+            tipos.add(new SelectItem("DBA"));
+        }
+        return tipos;
+    }
+
+    private void carregarListaUsuario() {
+        try {
+            usuarios = usuarioDAO.findAll();
+        } catch (Exception ex) {
+            addMensagem("Erro ao carregar lista Usuário.", FacesMessage.SEVERITY_WARN);
+        }
+    }
+    
+    public void novo(){
+        usuario = new Usuario();        
+    }
+
     public void salvar() {
         try {
-            usuarioDAO.salvar(usuario);
-            addMensagem("Cadastro realizado.", FacesMessage.SEVERITY_INFO);
+            if (usuario.getIdUsuario() == null || usuario.getIdUsuario() == 0) {                
+                usuarioDAO.salvar(usuario);                
+                addMensagem("Cadastro realizado.", FacesMessage.SEVERITY_INFO);
+            } else {                
+                usuarioDAO.alterar(usuario);
+                addMensagem("Cadastro alterado.", FacesMessage.SEVERITY_INFO);
+            }
+            novo();
+            carregarListaUsuario();            
         } catch (Exception ex) {
             addMensagem("Erro.", FacesMessage.SEVERITY_INFO);
             Logger.getLogger(UsuarioMB.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public void excluir(Usuario u) {
 
     }
 
+    public void tabCadastro(int id) throws Exception {
+        tab = "1";
+        usuario = usuarioDAO.consultar(id);
+    }
 }
