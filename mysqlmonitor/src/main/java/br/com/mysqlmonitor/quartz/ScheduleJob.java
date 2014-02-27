82057159741d12a573a438a8c0f4445dfc032332
orlandojsjr <orlandojsjr@users.noreply.github.com>
2014-02-27 13:39:19 +0000

    Adicionado quartz.

---
+++ mysqlmonitor/src/main/java/br/com/mysqlmonitor/quartz/ScheduleJob.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.mysqlmonitor.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ *
+ * @author orlando
+ */
+public class ScheduleJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jec) throws JobExecutionException {
+
+    }
+}
