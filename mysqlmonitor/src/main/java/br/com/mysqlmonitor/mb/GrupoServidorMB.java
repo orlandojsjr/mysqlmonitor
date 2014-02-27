e7c70a67fc9df591ea2a7d7e81cff947aa31df72
orlandojsjr <orlandojsjr@users.noreply.github.com>
2014-02-27 13:30:14 +0000

    Alteracao para alterar

--- mysqlmonitor/src/main/java/br/com/mysqlmonitor/mb/GrupoServidorMB.java
+++ mysqlmonitor/src/main/java/br/com/mysqlmonitor/mb/GrupoServidorMB.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.mysqlmonitor.mb;
 
 import br.com.mysqlmonitor.Interceptor.Login;
 import com.mysqlmonitor.dao.GrupoServidorDAO;
 import com.mysqlmonitor.entidade.GrupoServidor;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.inject.Model;
@@ -20,12 +20,15 @@ import javax.inject.Inject;
  * @author Orlando
  */
 @Login
-@Model 
-public class GrupoServidorMB extends Face{
+@Model
+public class GrupoServidorMB extends Face {
+
     @Inject
     private GrupoServidorDAO grupoServidorDAO;
     @Inject
     private GrupoServidor grupoServidor;
+    private List<GrupoServidor> grupoServidores;
+    private String tab;
 
     public GrupoServidor getGrupoServidor() {
         return grupoServidor;
@@ -34,15 +37,56 @@ public class GrupoServidorMB extends Face{
     public void setGrupoServidor(GrupoServidor grupoServidor) {
         this.grupoServidor = grupoServidor;
     }
-    
-    public void salvar(){
+
+    public List<GrupoServidor> getGrupoServidores() {
+        if (grupoServidores == null) {
+            carregarGrupoServidores();
+        }
+        return grupoServidores;
+    }
+
+    public void setGrupoServidores(List<GrupoServidor> grupoServidores) {
+        this.grupoServidores = grupoServidores;
+    }
+
+    public String getTab() {
+        return tab;
+    }
+
+    public void setTab(String tab) {
+        this.tab = tab;
+    }
+
+    private void carregarGrupoServidores() {
         try {
-            grupoServidorDAO.salvar(grupoServidor);
-            addMensagem("Cadastro realizado com sucesso!", FacesMessage.SEVERITY_INFO);
+            grupoServidores = grupoServidorDAO.findAll();
+        } catch (Exception e) {
+            addMensagem("Erro ao carregar", FacesMessage.SEVERITY_ERROR);
+        }
+    }
+
+    public void salvar() {
+        try {
+            if (grupoServidor.getIdGrupoServidor() == null || grupoServidor.getIdGrupoServidor() == 0) {
+                grupoServidorDAO.salvar(grupoServidor);
+                addMensagem("Cadastro realizado com sucesso!", FacesMessage.SEVERITY_INFO);
+            } else {
+                grupoServidorDAO.alterar(grupoServidor);
+                addMensagem("Cadastro alterado com sucesso!", FacesMessage.SEVERITY_INFO);
+            }
+            carregarGrupoServidores();
+            grupoServidor = new GrupoServidor();
         } catch (Exception ex) {
             addMensagem("Erro ao cadastrar!", FacesMessage.SEVERITY_ERROR);
             Logger.getLogger(GrupoServidorMB.class.getName()).log(Level.SEVERE, null, ex);
-        }        
+        }
+    }
+
+    public void excluir(GrupoServidor g){
+        
     }
     
+    public void tabCadastro() {
+        tab = "1";
+    }
 }
