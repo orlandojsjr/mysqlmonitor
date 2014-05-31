ec063412cef6d259e1fc4d9738be9266b9f82c7d
orlandojsjr <orlandojsjr@users.noreply.github.com>
2014-05-31 19:37:05 +0000

    Adicionado botoes iniciar e parar agente.

--- mysqlmonitor/src/main/java/br/com/mysqlmonitor/mb/IndexMB.java
+++ mysqlmonitor/src/main/java/br/com/mysqlmonitor/mb/IndexMB.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.mysqlmonitor.mb;
 
+import br.com.mysqlmonitor.interceptador.Login;
+import br.com.mysqlmonitor.quartz.ScheduleJob;
 import com.mysqlmonitor.entidade.Usuario;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
@@ -15,13 +16,27 @@ import javax.inject.Inject;
  * @author orlando
  */
 @Model
+@Login
 public class IndexMB {
-    
-    @Inject private Usuario usuario;
+
+    @Inject
+    private Usuario usuario;   
 
     public Usuario getUsuario() {
         return usuario;
-    }  
+    }
+
+    public void iniciarAgenteVerificadorCatalogos() {        
+        ScheduleJob.setRodarAgente(Boolean.TRUE);
+        System.out.println("Iniciando agente...");
+    }
+
+    public void pararAgenteVerificadorCatalogos() {        
+        ScheduleJob.setRodarAgente(Boolean.FALSE);
+        System.out.println("Parando agente...");
+    }
     
-    
+    public Boolean liberarBotao(){
+        return ScheduleJob.isRodarAgente();
+    }
 }
